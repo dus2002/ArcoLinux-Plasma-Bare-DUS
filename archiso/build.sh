cfb6deefe46e67f11daf34a39b92306b36e47838
Erik Dubois <erik.dubois@gmail.com>
2020-01-11 14:44:01 +0100

    comparing with official repos

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-comp
-iso_label="arcolinux-comp-v20.1.1"
+iso_label="arcolinux-comp-v20.2.1"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.1.1"
+iso_version="v20.2.1"
 install_dir=arch
 work_dir=work
 out_dir=out
