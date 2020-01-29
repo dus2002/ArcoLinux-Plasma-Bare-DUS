95b3797dff584ecacd445c9bbf8a07a49974908c
Erik Dubois <erik.dubois@gmail.com>
2020-01-29 08:00:42 +0100

    bump nr for welcome app

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-comp
-iso_label="arcolinux-comp-v20.2.8"
+iso_label="arcolinux-comp-v20.2.9"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.2.8"
+iso_version="v20.2.9"
 install_dir=arch
 work_dir=work
 out_dir=out
