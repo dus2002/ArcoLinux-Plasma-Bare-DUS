74c9bc3e33c532b86d9621b1b511a0802809d8d1
dus2002 <daniurdi46@gmail.com>
2020-03-03 21:27:33 +0100

    Final fix for Calamares

--- archiso/airootfs/usr/local/bin/calamares.sh
+++ archiso/airootfs/usr/local/bin/calamares.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-systemctl remove calamares
+systemctl disable calamares
+rm /etc/systemd/system/calamares.service
 rm -Rf /etc/calamaresmod
 rm -Rf /etc/calamares
 rm /usr/local/bin/calamares.sh
