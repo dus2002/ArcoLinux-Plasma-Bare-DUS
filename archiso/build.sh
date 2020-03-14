db0f1dd9d068e6b3bf6e48f58951bc8fba4a65ae
dus2002 <daniurdi46@gmail.com>
2020-03-14 14:45:30 +0100

    Don't remove airootfs, until ISO creation didn't finish

--- archiso/build.sh
+++ archiso/build.sh
@@ -193,7 +193,6 @@ make_prepare() {
     cp -a -l -f ${work_dir}/x86_64/airootfs ${work_dir}
     mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" pkglist
     mkarchiso -c lzo ${verbose} -w "${work_dir}" -D "${install_dir}" ${gpg_key:+-g ${gpg_key}} prepare
-    rm -rf ${work_dir}/airootfs
     # rm -rf ${work_dir}/x86_64/airootfs (if low space, this helps)
 }
 
